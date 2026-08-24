75a37faf3539c1afb8ebb9aa5d3a16e32aff434d
Alejandro Gutierrez <c.gutierrez.d.alejandro@gmail.com>
2026-08-23 23:03:43 -0500

    deploy

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,14 @@ import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: (process.env.CORS_ORIGIN ?? 'http://localhost:3001')
+      .split(',')
+      .map((origin) => origin.trim()),
+    methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
